bde275bdb0e732b9ad8bb11866992f07ba2d2a54
Rajat Patel <77857102+RajatPatel1112@users.noreply.github.com>
2022-12-09 11:40:53 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,10 @@
+#Make use of print() 
+print("Rajat")
+print("30 November 2022")
+print("I am signing up for 100 days of python challenge!")
+print("""I will make sure to spend some time every day coding along for a minimum of 10 minutes a day.
+I'll be using an amazing online IDE so Ican do
+this from my phone wherever I happen to be. No xcuses
+for not coding from the middle of a field!""")
+print("I am felling👌")
+print("You can follow my progress at replit.com/@RajatPatel2")
