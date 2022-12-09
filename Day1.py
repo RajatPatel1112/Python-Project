b1ad7224e8285532b5be2205b212dd060572ab73
Rajat Patel <77857102+RajatPatel1112@users.noreply.github.com>
2022-12-09 23:14:18 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,10 +1,20 @@
-#Make use of print() 
+#Make use of print() function
+"""
+Day1 Challenge
+Write your full name and today's date in separate lines of code.
+Copy this text below. See if you can do it with just the one print statement!
+"I am signing up for 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+No excuses for not coding from the middle of a field!
+On the next line add I am feeling with an emoji of your choice to illustrate how you're feeling about the challenge."
+
+Add one more line to your output You can follow my progress at replit.com/@ and add in your Replit username. 
+This will be a great way of maintaining accountability for yourself! or your github.
+"""
 print("Rajat")
 print("9 December 2022")
 print("I am signing up for 100 days of python challenge!")
 print("""I will make sure to spend some time every day coding along for a minimum of 10 minutes a day.
-I'll be using an amazing online IDE so Ican do
-this from my phone wherever I happen to be. No xcuses
-for not coding from the middle of a field!""")
+No excuses for not coding from the middle of a field!""")
 print("I am felling👌")
-print("You can follow my progress at replit.com/@RajatPatel2")
+print("You can follow my progress at replit: https://replit.com/@RajatPatel2 and github: https://github.com/RajatPatel1112"
