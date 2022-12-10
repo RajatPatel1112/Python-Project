b5fdef917c539ff19783a9156c2d72de780e5629
Rajat Patel <77857102+RajatPatel1112@users.noreply.github.com>
2022-12-10 11:02:08 +0530

    Create Day2-input()_and_variables.py

---
+++ Day2-input()_and_variables.py
@@ -0,0 +1,19 @@
+#Making use of input() function and creating variables
+"""
+Day2 Challenge
+Asks for the user's name, favorite food, favorite music and where they live (or you can create other questions!)
+Store all of the answers in different variables.
+Print out a full sentence that includes the user's favorite things.
+Give them a positive affirmation at the end - tell them they rock at doing something!
+"""
+
+
+
+name = input("What's your name? : ")
+food = input("What's your favourite food? : ")
+music = input("What's your favourite music? : ")
+place = input("Where do you live? : ")
+print("you are",name)
+print("you are probably hungry for",food)
+print("and you're definitely getting your grooves on to",music)
+print("living in the amazing",place)
