1c1e8d1099a2b9f1fb28cdb2599ffa3e94a0e3d5
Rajat Patel <77857102+RajatPatel1112@users.noreply.github.com>
2022-12-12 13:54:43 +0530

    Update Day4-change_textcolor.py

--- Day4-change_textcolor.py
+++ Day4-change_textcolor.py
@@ -1,5 +1,6 @@
 #Color print
 """
+Day4 challenge
 1. Ask your users to list a bunch of information about them: things they like, things they hate, names of family and friends... 
    it's up to you how many and what kinds of things you pick. Keep it wacky!
 2. Now construct your story - it can be about anything you want, but must use the variables you've created in step 1.
