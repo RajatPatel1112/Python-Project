edfcf6404a239a9f428d04dcba2ce2bf8e8f9831
Rajat Patel <77857102+RajatPatel1112@users.noreply.github.com>
2022-12-13 11:08:33 +0530

    Create Day5-if_else.py

---
+++ Day5-if_else.py
@@ -0,0 +1,32 @@
+#Use of if else statement.
+"""
+Day5 Challenge
+Ask your users a series of questions that identify if they're one of the characters in the world you have created.
+Add multiple if statements to check the result of each question.
+Make sure to have a final print if they haven't selected any of the characters so far.
+"""
+
+
+
+
+
+print("Anime Character's Likes")
+print("--")
+
+naruto = input("Do you like Ramen? : ")
+if naruto == "yes":
+  print("You are Naruto")
+else:
+  print("You are not naruto")
+
+luffy = input("Do you like meat? : ")
+if luffy == "yes":
+  print("You are luffy")
+else:
+  print("You are not luffy")
+
+nami = input("Do you like treaure? : ")
+if nami == "yes":
+  print("You are nami")
+else:
+  print("You are not nami")
